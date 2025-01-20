81cccd7711bcf0cf1e2631b7237dbca0b83e5ccb
cgHome <cghome@cflat-inc.org>
2025-01-20 15:07:36 +0100

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 'use strict';
 
-module.exports = require('./lib/telemetryCollectorApi');
+const TelemetryCollectorApi = require('./lib/telemetryCollectorApi')
+
+const { TELCO_LOGLEVEL } = require('./lib/telcoConstants');
+
+module.exports = { TelemetryCollectorApi, TELCO_LOGLEVEL }
